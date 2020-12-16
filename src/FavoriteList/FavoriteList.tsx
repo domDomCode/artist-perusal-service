06af22377b15e4ae68b19a062c26b6ae3ec4a85d
domDom <domin.stasiak@gmail.com>
2020-12-16 19:46:14 +0100

    FavoriteList styles

--- src/FavoriteList/FavoriteList.tsx
+++ src/FavoriteList/FavoriteList.tsx
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
 import StoreContext from "../StoreContext/StoreContext";
-import { List, ListItem } from "@material-ui/core";
+import { useHistory } from 'react-router-dom';
+import { List, ListItem, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
+
+import './FavoriteList.scss'
 
 export interface FavoriteArtistInterface {
   name: string,
@@ -11,23 +14,25 @@ export interface FavoriteArtistInterface {
 const FavoriteList = () => {
   const store = useContext(StoreContext);
 
+  const history = useHistory();
+
 
   const favoriteList: FavoriteArtistInterface[] | [] = store.favoriteList ? store.favoriteList.get : [];
 
   return (
-    <>
+    <div className={'FavoriteList'}>
       {favoriteList.length
         ? <List>
           {favoriteList.map((favorite: FavoriteArtistInterface) => (
-            <ListItem key={favorite.mbid}>
-              <Link to={`/artist/${favorite.mbid}`}>
-                {favorite.name}
-              </Link>
+            <ListItem button key={favorite.mbid} onClick={() => history.push(`/artist/${favorite.mbid}`)}>
+                <Typography variant={"body1"}>{favorite.name}</Typography>
             </ListItem>
           ))}
         </List>
-        : <div>You do not have any favorite artists... yet! Look one up using the search on the right</div>}
-    </>
+        : <Typography variant={"body1"}>
+          You don't have any favorite artists... yet! Look one up using the search on the right.
+        </Typography>}
+    </div>
   )
 }
 
